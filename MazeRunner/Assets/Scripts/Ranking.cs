8f1d976122925e73a77b4cff76cb8821928b7a2d
Trung Nguyen <twinsdra@gmail.com>
2016-11-15 03:26:46 -0500

    Added simple distance to goal ranking

---
+++ MazeRunner/Assets/Scripts/Ranking.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class Ranking : MonoBehaviour {
+
+    // Use this for initialization
+    private SortedList list = new SortedList();
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var ranking = GetComponent<Text>();
+        ranking.text = "";
+        foreach (var item in list.GetValueList())
+        {
+            ranking.text = ranking.text + item + "\n";
+        }
+    }
+    public void add(float number, string name)
+    {
+        if (!list.ContainsValue(name))
+        {
+            while (list.ContainsKey(number)) number -= 0.001f;
+            list.Add(number, name);
+        } else
+        {
+            int index = list.IndexOfValue(name);
+            list.RemoveAt(index);
+            while (list.ContainsKey(number)) number -= 0.001f;
+            list.Add(number, name);
+        }
+    }
+}
---
+++ MazeRunner/Assets/Scripts/ReportDistance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ReportDistance : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject finish;
+    public Ranking ranking;
+    void Start () {
+        finish = GameObject.Find("Finish");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(finish.transform.position, transform.position);
+        ranking.add(dist, this.name);
+    }
+}
