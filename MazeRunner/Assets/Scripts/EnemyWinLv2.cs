ab1df0e89ab188a6f148f25734e13a32b6bf2713
Brandon <b.chims@gmail.com>
2016-11-30 00:33:19 -0500

    Fixed Character Control. Added upright anim.
    Character control fixes: Turning in place, hovering after jumping,
    dynamic control (shift to walk, c to crouch)
    Also with this push is changes to projectVersion and RainProjectState
    which I did not touch.... so if anything breaks after this, I apologize.

---
+++ MazeRunner/Assets/Scripts/EnemyWinLv2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnemyWinLv2 : MonoBehaviour {
+
+void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Enemy1" || col.gameObject.name == "Enemy2" || col.gameObject.name == "Enemy3") {
+			Scene scene = SceneManager.GetActiveScene(); 
+			SceneManager.LoadScene(scene.name);
+		}
+		else if(col.gameObject.name == "Player") {
+			SceneManager.LoadScene("Level3");
+		}
+	}
+}
