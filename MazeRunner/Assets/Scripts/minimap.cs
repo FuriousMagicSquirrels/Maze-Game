b27f875f70967f68e966abd51fbdafa9b44134d9
chaobest <chaobest@hotmail.com>
2016-12-08 12:47:47 -0500

    1

---
+++ MazeRunner/Assets/Scripts/minimap.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class minimap : MonoBehaviour
+{
+
+
+
+    void Awake()
+    {
+        Transform fire = GameObject.Find("Campfire").transform;
+        if (fire.Find("CampfireCollider").GetComponent<LevelChanger>().loseEnable)
+        {
+            DontDestroyOnLoad(gameObject);
+            Debug.Log("reload");
+        }
+
+    }
+}
